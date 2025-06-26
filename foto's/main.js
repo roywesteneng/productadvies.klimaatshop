57aba51b37ee74bb1cb8a5147c6581372ac0939e
roywesteneng <roy@klimaatshop.nl>
2025-06-26 17:28:22 +0200

    Delete foto's directory

--- foto's/main.js
+++
@@ -1,5 +0,0 @@
- function scrollSlider(direction) {
-      const slider = document.getElementById('productSlider');
-      const scrollAmount = 300;
-      slider.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
-    }
